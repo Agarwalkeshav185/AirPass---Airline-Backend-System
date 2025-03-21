2c82baa39d36fd5973cb96bc013746d569bed6a1
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-03-21 23:21:38 +0530

    Added create booking repository code

--- BookingService/src/migrations/20250321143309-create-booking.js
+++ BookingService/src/migrations/20250321143309-create-booking.js
@@ -24,6 +24,7 @@ module.exports = {
         values: ['InProcess', 'Booked', 'Cancelled'],
         defaultValue:'InProcess'
       },
+      
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
