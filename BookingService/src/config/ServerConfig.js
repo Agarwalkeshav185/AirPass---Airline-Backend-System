0d5ed41d1b1118ccf763d8140467e17ab5349239
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-03-21 19:35:49 +0530

    First commit with basic setup of Booking Service

---
+++ BookingService/src/config/ServerConfig.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+module.exports = {
+    PORT : process.env.PORT,
+    DB_SYNC : process.env.DB_SYNC,
+    
+
+}
--- BookingService/src/index.js
+++ BookingService/src/index.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const {PORT} = require('./config/ServerConfig');
+const bodyParser = require('body-parser');
+const db = require('./models/index')
+const apiRoutes = require('./Routes/index');
+
+const app = express();
+
+const setupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`ServerStarted on Port:- ${PORT}`);
+        if(DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    })
+}
+
+setupAndStartServer();
