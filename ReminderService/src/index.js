d0dd89d69b94af484a93896ffdabf9462ef42ec9
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-03-22 23:18:08 +0530

    Added More Logic to the reminder service

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
-
+const jobs = require('./utils/jobs');
+const TicketController = require('./controllers/ticket-controller');
 
 const setUpAndStartServer = async () =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
+    app.post('/api/v1/tickets', TicketController.create);
     app.listen(PORT, ()=>{
         console.log(`Server started at Port :- ${PORT}`);        
-        
+        jobs();
     });
 
 }
--- ReminderService/src/services/email-service.js
+++ ReminderService/src/services/email-service.js
@@ -1,5 +1,7 @@
 const sender = require('../config/email-config');
+const TicketRepository = require('../repository/ticket-repository');
 
+const repo = new TicketRepository();
 const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) =>{
     try {
         const response = sender.sendMail({
@@ -13,9 +15,38 @@ const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) =>{
     }
 }
 
+const fetchPendingEmails = async (timeStamp) =>{
+    try {
+        const response = await repo.get({status : "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNotification = async (data) =>{
+    try {
+        const ticket = await repo.create(data);
+        return ticket;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+const updateTicket = async(ticketId, data) =>{
+    try {
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
 module.exports = {
     sendBasicEmail,
-
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
 /**
  * SMTP --> a@b.com
