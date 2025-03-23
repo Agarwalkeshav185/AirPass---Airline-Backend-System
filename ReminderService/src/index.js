6c47d33194b8699de20e98c1ca0f7d0b8b31b321
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-03-23 22:02:53 +0530

    Added Logic to start the Message Queue Setup

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {PORT} = require('./config/serverConfig');
-const { createChannel } = require('./utils/messageQueue');
-const jobs = require('./utils/jobs');
+const {PORT , REMINDER_BINDING_KEY} = require('./config/serverConfig');
+const { createChannel, subscribeMessage } = require('./utils/messageQueue');
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
+const jobs = require('./utils/jobs');
 
 const setUpAndStartServer = async () =>{
     const app = express();
@@ -12,12 +13,13 @@ const setUpAndStartServer = async () =>{
     app.use(bodyParser.urlencoded({extended:true}));
 
     const channel = await createChannel();
+    await subscribeMessage(channel, EmailService.SubscribeEvents , REMINDER_BINDING_KEY);
 
     app.post('/api/v1/tickets', TicketController.create);
     
     app.listen(PORT, ()=>{
-        console.log(`Server started at Port :- ${PORT}`);        
-        jobs();
+        console.log(`Server started at Port :- ${PORT}`);  
+        jobs();      
     });
 
 }
