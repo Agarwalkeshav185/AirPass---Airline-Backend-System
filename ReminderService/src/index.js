fe8fc5b0a6b02fc2e938dd977f93198f5947d094
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-03-23 15:33:37 +0530

    Added MessageQueue Setup to the Reminder system

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
+const { createChannel } = require('./utils/messageQueue');
 const jobs = require('./utils/jobs');
 const TicketController = require('./controllers/ticket-controller');
 
@@ -10,7 +11,10 @@ const setUpAndStartServer = async () =>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    const channel = await createChannel();
+
     app.post('/api/v1/tickets', TicketController.create);
+    
     app.listen(PORT, ()=>{
         console.log(`Server started at Port :- ${PORT}`);        
         jobs();
